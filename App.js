d5a94d5a779ee6f4f05de12892a655849d85fa7f
thetomcoffee <tom@tomswebsite.org>
2021-07-24 18:10:17 -0500

    Added name prompt, and text input for name.

--- App.js
+++ App.js
@@ -1,55 +1,63 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, TextInput } from 'react-native';
 
 const App = () => {
   const [currentDate, setCurrentDate] = useState('');
 
   useEffect(() => {
     let rightNow = new Date();
-    let date = rightNow.getDate(); 
-    let month = rightNow.getMonth() + 1; 
-    let year = rightNow.getFullYear(); 
-    let hours = rightNow.getHours(); 
-    let min = rightNow.getMinutes(); 
-    let sec = rightNow.getSeconds(); 
+    let date = rightNow.getDate();
+    let month = rightNow.getMonth() + 1;
+    let year = rightNow.getFullYear();
+    let hours = rightNow.getHours();
+    let min = rightNow.getMinutes();
+    let sec = rightNow.getSeconds();
     let amOrPm = '';
     let minZero = '';
     let secZero = '';
 
     if (hours >= 12) {
-        amOrPm = 'PM';
-        if (hours > 12)
-            hours = hours - 12; 
-    } 
-    else 
-        amOrPm = 'AM';
+      amOrPm = 'PM';
+      if (hours > 12)
+        hours = hours - 12;
+    }
+    else
+      amOrPm = 'AM';
 
-    if(min < 10)
-        minZero = '0';
+    if (min < 10)
+      minZero = '0';
 
-    if(sec < 10)
-        secZero = '0'
+    if (sec < 10)
+      secZero = '0'
 
     setCurrentDate(
-      month + '/' + date + '/' + year 
+      month + '/' + date + '/' + year
       + ' ' + hours + ':' + minZero + min + ':' + secZero + sec + ' ' + amOrPm
     );
   }, []);
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-            <Text style={styles.textStyle}>
-                Hello, and welcome to the Awesome Project! 
-            </Text>
-          <Text style={styles.textStyle}>
-            Current Date Time
-          </Text>
-          <Text style={styles.textStyle}>
-            {currentDate}
-          </Text>
-
+        <Text style={styles.textStyle}>
+          Hello, and welcome to the Awesome Project!
+        </Text>
+        <Text style={styles.textStyle}>
+          Current Date Time
+        </Text>
+        <Text style={styles.textStyle}>
+          {currentDate}
+        </Text>
+        <Text>What's your name? Please enter it below:</Text>
+        <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="Name"
+        />
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
